58997aae895ec40f8cf775366bc7ba723158569d
Deepstud <2928855@hc.ac.je>
2024-10-24 11:55:55 +0100

    New CSS and JavaScript elements

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // javascript
 
 alert("Hello from index.js!");
+
+// Beginners 5 hours per week.
+// Advanced 10 hours per week.
+// Won't input form unless  1 hour is selected.
+//Show a summary of the cost- I am thinking of adding a text box, that will change based upon the selected level.
+//Show a summary of the order.  A separate box, which acts a receipt would work.
+//Currency is in GBP
+//Hours aren't negative
+//Show correct costs for the level
+
+//Main task to validation rules such as ensure that the input is valid. Email check, selected hours are correct.
